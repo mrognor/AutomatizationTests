05dce71bd015843feb00217f09869eb6d5dbbf78
mrognor <47296449+mrognor@users.noreply.github.com>
2023-11-28 14:39:17 +0300

    Update Gendoc.sh

--- Docs/Gendoc.sh
+++ Docs/Gendoc.sh
@@ -1,5 +1,5 @@
 # Add main page doxygen command to ReadMe.md
-cd ..
+cd $(dirname "$0")/..
 mv ReadMe.md TmpReadMe.md
 echo '\mainpage' > ReadMe.md
 cat TmpReadMe.md >> ReadMe.md
@@ -7,7 +7,7 @@ rm TmpReadMe.md
 
 # Create documentation
 mv Docs/Doxyfile Doxyfile
-doxygen Doxyfile
+# doxygen Doxyfile
 mv Doxyfile Docs/Doxyfile
 
 # Remove main page doxygen command from ReadMe.md
